e9b84dad4623e65a40938d34d908c7c65fd71c0a
Alfredo Montoya <fredo.montoya@gmail.com>
2019-06-10 23:12:17 -0500

    Mac: Added more to two existing files and created new file mac2-feat

--- mac_created1.py
+++ mac_created1.py
@@ -1,2 +1,7 @@
 x = 1
 print(x)
+
+y = ['a', 'b', 'c', 'd']
+for i in y:
+	print(i)
+
--- test.py
+++ test.py
@@ -4,3 +4,7 @@ print("Even More Words from Mac")
 print("I have even more to say from Mac")
 
 print("Final Note: I'm done")
+
+
+'''New Branch Code Starts Here'''
+print("P.S. This was added in a new branch")
